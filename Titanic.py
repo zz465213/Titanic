116759f06eaf01e7d6f9e53f024d6fab84406de7
Leon Wang <Leon>
2023-02-03 23:40:42 +0800

    merge

--- Titanic.py
+++ Titanic.py
@@ -1,6 +1,10 @@
 import pandas as pd
+import numpy
 #讀入.csv
-df1 = pd.read_excel('C:/Big Data/Competitions/Titanic/test.csv')
-df2 = pd.read_excel('C:/Big Data/Competitions/Titanic/gender_submission.csv')
+df1 = pd.read_csv('C:/Big Data/Competitions/Titanic/test.csv')
+df2 = pd.read_csv('C:/Big Data/Competitions/Titanic/gender_submission.csv')
 #多表連結 用test.csv為基底
-pd.concat([df1,df2], join='left', ignore_index=True).to_csv('C:/Big Data/Competitions/Titanic/left.csv')
+# concat就是將兩個資料直接合併
+# pd.concat([df1,df2], join='inner', ignore_index=True).to_csv('C:/Big Data/Competitions/Titanic/inner.csv')
+# merge自己找共有項
+inner_join=pd.merge(left=df1,right=df2,how='inner').to_csv('C:/Big Data/Competitions/Titanic/inner.csv')
