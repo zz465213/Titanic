23fbe6a85b80659a9678618aeec2e1db154076d6
Leon Wang <Leon>
2023-02-04 16:29:43 +0800

    test

--- Titanic.py
+++ Titanic.py
@@ -4,7 +4,7 @@ import numpy
 df1 = pd.read_csv('C:/Big Data/Competitions/Titanic/test.csv')
 df2 = pd.read_csv('C:/Big Data/Competitions/Titanic/gender_submission.csv')
 #多表連結 用test.csv為基底
-# concat就是將兩個資料直接合併
-# pd.concat([df1,df2], join='inner', ignore_index=True).to_csv('C:/Big Data/Competitions/Titanic/inner.csv')
-# merge自己找共有項
+# concat->將兩DataFrame直接合併
+# pd.concat([df1,df2], join='outer', ignore_index=True).to_csv('C:/Big Data/Competitions/Titanic/concat_out.csv')
+# merge->依照某列合併兩個DataFrame
 inner_join=pd.merge(left=df1,right=df2,how='inner').to_csv('C:/Big Data/Competitions/Titanic/inner.csv')
